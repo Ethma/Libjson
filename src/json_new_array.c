2918bdd765469d96c4d6130606161cccc9811eb7
mabessir <mabessir@student.42.fr>
2018-08-31 12:41:49 +0200

    First push for new_array

--- src/json_new_array.c
+++ src/json_new_array.c
@@ -6,7 +6,24 @@
 /*   By: mabessir <mabessir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 15:50:36 by mabessir          #+#    #+#             */
-/*   Updated: 2018/08/30 15:50:38 by mabessir         ###   ########.fr       */
+/*   Updated: 2018/08/31 12:39:18 by mabessir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/json.h"
+#include "../includes/libc.h"
+
+t_json_string *make_new_array(t_json_file *file)
+{
+	t_json_array *tab;
+
+}
+
+t_json_value *new_array(t_json_file *file, t_json_value *parent)
+{
+	t_json_array *new_array;
+
+	if ((new_array = make_new_array(file)) == NULL)
+		return (NULL);
+	return (ft_fill_json_value(parent, array, new_array));
+}
