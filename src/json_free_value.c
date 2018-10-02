dfc3d0e60e5297fcccb1036066dd0770991b7ff3
mabessir <mabessir@student.42.fr>
2018-10-02 12:21:20 +0200

    test

--- src/json_free_value.c
+++ src/json_free_value.c
@@ -6,7 +6,7 @@
 /*   By: mabessir <mabessir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 16:17:57 by mabessir          #+#    #+#             */
-/*   Updated: 2018/09/27 16:57:12 by mabessir         ###   ########.fr       */
+/*   Updated: 2018/10/02 12:20:49 by mabessir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,20 @@ void	*json_free_string(void **ptr)
 		return (NULL);
 	ft_free(string->str);
 	ft_free(string);
+	ft_free(val);
+	return (NULL);
+}
+
+void	*json_free_number(void *ptr)
+{
+	t_json_value *val;
+	double	*num;
+
+	if (!(val = (t_json_value *)(*ptr)))
+		return(NULL);
+	if (!(num = (double *)val->ptr))
+		retrun (NULL);
+	ft_free(num);
 	ft_free(val);
 	return (NULL);
 }
